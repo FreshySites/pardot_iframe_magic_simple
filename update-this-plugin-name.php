9bd19c894d7bb1201abb2a140956406b3ab993b5
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2022-08-12 15:44:04 -0400

    Update update-this-plugin-name.php

--- update-this-plugin-name.php
+++ update-this-plugin-name.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* Plugin Name: Add PluginName
-* Plugin URI: https://github.com/FreshyMichael/Plugin-Starter
-* Description: Add a Description
+* Plugin Name: Include Any Page
+* Plugin URI: https://github.com/FreshyMichael/include-any-page
+* Description: Include the full output of any exterior page with a simple shortcode that accepts a URL
 * Version: 1.0.0
 * Author: FreshySites
 * Author URI: https://freshysites.com/
@@ -17,6 +17,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 //______________________________________________________________________________
 
 
+add_shortcode('include_any_page', 'file_get_any_page_landing');
+
+
+
+function file_get_any_page_landing($atts){
+
+	ob_start();
+	
+	extract(shortcode_atts(array('url' =>'*'), $atts));
+	
+	return file_get_contents($url);
+
+	$ReturnString = ob_get_contents();
+		
+	ob_end_clean();
+	
+	return $ReturnString;
+
+}
 //______________________________________________________________________________
 // All About Updates
 
@@ -24,10 +43,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 // ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
-	'https://github.com/FreshyMichael/Plugin-Starter',
+	'https://github.com/FreshyMichael/include-any-page',
 	__FILE__,
 // ***IMPORTANT*** Update this to New Repository Master Branch Path
-	'Plugin-Starter'
+	'include-any-page'
 );
 //Enable Releases
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
